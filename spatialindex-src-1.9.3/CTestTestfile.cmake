8d4a8497a52e1ada589337ffb6b543e318249190
jamesyang2333 <jamesyang2333@gmail.com>
2022-10-15 13:41:18 +0000

    initial commit

---
+++ spatialindex-src-1.9.3/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/jingyi2/platon/spatialindex-src-1.9.3
+# Build directory: /home/jingyi2/platon/spatialindex-src-1.9.3
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
---
+++ spatialindex-src-1.9.3/src/CMakeFiles/spatialindex_c.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spatialindex_c.dir/capi/BoundsQuery.cc.o"
+  "CMakeFiles/spatialindex_c.dir/capi/CountVisitor.cc.o"
+  "CMakeFiles/spatialindex_c.dir/capi/CustomStorage.cc.o"
+  "CMakeFiles/spatialindex_c.dir/capi/DataStream.cc.o"
+  "CMakeFiles/spatialindex_c.dir/capi/Error.cc.o"
+  "CMakeFiles/spatialindex_c.dir/capi/IdVisitor.cc.o"
+  "CMakeFiles/spatialindex_c.dir/capi/Index.cc.o"
+  "CMakeFiles/spatialindex_c.dir/capi/LeafQuery.cc.o"
+  "CMakeFiles/spatialindex_c.dir/capi/ObjVisitor.cc.o"
+  "CMakeFiles/spatialindex_c.dir/capi/sidx_api.cc.o"
+  "CMakeFiles/spatialindex_c.dir/capi/Utility.cc.o"
+  "../bin/libspatialindex_c.pdb"
+  "../bin/libspatialindex_c.so.6.1.1"
+  "../bin/libspatialindex_c.so"
+  "../bin/libspatialindex_c.so.6"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spatialindex_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
